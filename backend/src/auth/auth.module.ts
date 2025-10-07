be9fbae673c84b2246ca4de5d6e6c2707cd8ec2d
Tiffany Nebo <nebotiffany@gmail.com>
2025-10-07 14:22:26 +0100

    feat(auth): Add basic auth module with JWT strategy

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+// import { PrismaService } from './prisma.service';
+import { JwtStrategy } from './jwt.strategy';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'supersecret', // use .env in prod
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService, PrismaService, JwtStrategy],
+  controllers: [AuthController],
+})
+export class AuthModule {}
